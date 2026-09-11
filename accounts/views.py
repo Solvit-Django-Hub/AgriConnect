6da09ce7bd971cae5818ad0f0e9a6e75ee089ec8
MASENGESHO Odile <masengeshoodile@gmail.com>
2026-09-11 13:01:17 +0200

    Implement user registration API

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserRegistrationSerializer
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+
+            return Response(
+                {
+                    "message": "User registered successfully.",
+                    "user": {
+                        "id": user.id,
+                        "username": user.username,
+                        "email": user.email,
+                        "phone_number": user.phone_number,
+                        "role": user.role,
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
--- agriconnect/settings.py
+++ agriconnect/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third parties
+    'rest_framework', 
+
+    #Apps
     'farmers',
     'buyers',
     'orders',
